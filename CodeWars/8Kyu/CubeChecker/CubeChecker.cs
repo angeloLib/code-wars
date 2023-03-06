a6f23c2354ad3371b1ba99e8a52c067e4983b030
angeloLib <angelo.liborio.dev@gmail.com>
2023-03-06 00:41:06 +0000

    Add project files.

---
+++ CodeWars/8Kyu/CubeChecker/CubeChecker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CodeWars._8Kyu.CubeChecker
+{
+    public class CubeCheck
+    {
+        public bool IsCube(double volume, double side)
+        {
+            return (Math.Pow(side, 3) == volume && side > 0);
+        }
+    }
+}
