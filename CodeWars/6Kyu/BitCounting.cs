9fc0ae2b7e38f790a986bfabc45fcb58de18aebd
angeloLib <angelo.liborio.dev@gmail.com>
2023-03-07 00:23:19 +0000

    - New exercise

---
+++ CodeWars/6Kyu/BitCounting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._6Kyu
+{
+    public class BitCounting
+    {
+        public int CountBits(int n)
+        {
+            if (n < 0) throw new ArgumentOutOfRangeException("n", "Input value must be an non negative integer.");
+            return CountOnes(Convert.ToString(n, 2));
+        }
+
+        private int CountOnes(string s)
+        {
+            char[] chars = s.ToCharArray();
+            int count = 0;
+            foreach (char c in chars)
+            {
+                if (c == '1') count++;
+            }
+            return count;
+        }
+    }
+}
