de3f1dc8951b32352aa655842e640721dceb1af2
angeloLib <angelo.liborio.dev@gmail.com>
2023-03-08 02:03:16 +0000

    Decoder

---
+++ CodeWars/6Kyu/DuplicateEncoder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._6Kyu
+{
+    public class DuplicateEncoder
+    {
+        public string DuplicateEncode(string word)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            string result = string.Empty;
+            foreach (var letter in word.ToCharArray())
+            {
+                if (dic.ContainsKey(letter))
+                {
+                    dic[letter] = dic[letter] + 1;
+                }
+                else { dic.Add(char.ToLower(letter), 1); }
+            }
+            foreach (var letter in word.ToCharArray())
+            {
+                result = result + (dic[char.ToLower(letter)] > 1 ? ")" : "(");
+            }
+            return result;
+        }
+    }
+}
---
+++ CodeWarsTests/6Kyu/DuplicateEncoderTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars._6Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._6Kyu.Tests
+{
+    [TestClass()]
+    public class DuplicateEncoderTests
+    {
+        [TestMethod()]
+        public void BasicTests()
+        {
+            DuplicateEncoder duplicateEncoder = new DuplicateEncoder();
+            Assert.AreEqual("(((", duplicateEncoder.DuplicateEncode("din"));
+            Assert.AreEqual("()()()", duplicateEncoder.DuplicateEncode("recede"));
+            Assert.AreEqual(")())())", duplicateEncoder.DuplicateEncode("Success"), "should ignore case");
+            Assert.AreEqual("))((", duplicateEncoder.DuplicateEncode("(( @"));
+
+            //din = todos os chars são diferentes = (((
+            //recede = apenas os e's são iguais ()()()
+            //success = repete os S, logo )())())
+            //(( @ = logo  ))((  
+
+        }
+    }
+}
