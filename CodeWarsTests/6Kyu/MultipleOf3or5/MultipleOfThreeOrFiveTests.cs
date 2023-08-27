0fe11950c35b42e835d06d05352f26dbd15fb792
angeloLib <angelo.liborio.dev@gmail.com>
2023-08-27 23:50:22 +0100

    - splitString

--- CodeWarsTests/6Kyu/MultipleOf3or5/MultipleOfThreeOrFiveTests.cs
+++ CodeWarsTests/6Kyu/MultipleOf3or5/MultipleOfThreeOrFiveTests.cs
@@ -13,6 +13,7 @@ namespace CodeWars._6Kyu.Tests
             Assertion(expected: 78, input: 20);
             Assertion(expected: 9168, input: 200);
             Assertion(expected: 0, input: 0);
+
         }
 
         private static void Assertion(int expected, int input) =>
