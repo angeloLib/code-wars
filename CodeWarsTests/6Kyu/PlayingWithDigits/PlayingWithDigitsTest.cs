6ae834e3e57125f0880e55f0ecdf36f53e2624fd
angeloLib <angelo.liborio.dev@gmail.com>
2023-08-28 00:14:16 +0100

    PlayingWithDigits

---
+++ CodeWarsTests/6Kyu/PlayingWithDigits/PlayingWithDigitsTest.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars._6Kyu.MultipleOf3or5;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars._6Kyu.Tests
+{
+    [TestClass()]
+    public class PlayingWithDigitsTest
+    {
+        [TestMethod()]
+        public void BasicTests()
+        {
+            Assert.AreEqual(1, PlayingWithDigits.PlayingWithDigits.DigPow(89, 1));
+
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual(-1, PlayingWithDigits.PlayingWithDigits.DigPow(92, 1));
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual(51, PlayingWithDigits.PlayingWithDigits.DigPow(46288, 3));
+        }
+    }
+}
