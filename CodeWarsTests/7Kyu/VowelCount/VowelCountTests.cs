1cb9288dd3940f0a539f9d053a1b4ed050eeecb2
angeloLib <angelo.liborio.dev@gmail.com>
2023-08-28 00:03:04 +0100

    - VowelCount

---
+++ CodeWarsTests/7Kyu/VowelCount/VowelCountTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars._7Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeWars._7Kyu.VowelCount;
+
+namespace CodeWars._7Kyu.Tests
+{
+    [TestClass()]
+    public class VowelCountTests
+    {
+        [TestMethod]
+        public void TestCase1()
+        {
+            Assert.AreEqual(5, VowelCount.VowelCount.GetVowelCount("abracadabra"));
+        }
+    }
+}
