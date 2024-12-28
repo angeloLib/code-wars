ea1deb6e95ec9f819cb41df671c257f02e3fbad1
angeloLib <angelo.liborio.dev@gmail.com>
2024-12-28 13:54:42 +0000

    - sphere

---
+++ C#/CodeWars/7Kyu/BuildingSpheres/Sphere.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CodeWars._7Kyu.BuildingSpheres
+{
+    public class Sphere
+    {
+        public int Radius { get; private set; }
+        public int Mass { get; private set; }
+
+        private const double FourThirds = 4.0 / 3.0;
+
+        public Sphere(int radius, int mass)
+        {
+            Radius = radius;
+            Mass = mass;
+        }
+
+        public int GetRadius() { return Radius; }
+        public int GetMass() { return Mass; }
+
+        public double GetVolume()
+        {
+            double volume = FourThirds * Math.PI * Math.Pow(Radius, 3);
+            return Math.Round(volume, 5); // Round to 5 decimal places
+        }
+
+        public double GetSurfaceArea()
+        {
+            double area = 4 * Math.PI * Math.Pow(Radius, 2);
+            return Math.Round(area, 5); // Round to 5 decimal places
+        }
+
+        public double GetDensity()
+        {
+            double volume = GetVolume(); // Already rounded
+            double density = Mass / volume;
+            return Math.Round(density, 5); // Round to 5 decimal places
+        }
+    }
+}
---
+++ C#/CodeWarsTests/7Kyu/BuildingSpheres/SphereTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars._7Kyu.BuildingSpheres;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWarsTests._7Kyu.BuildingSpheres
+{
+    [TestClass]
+    public class SphereTests
+    {
+        [TestMethod]
+        public static void ExampleTest()
+        {
+            Sphere ball = new Sphere(2, 50);
+            Assert.AreEqual(2.0, ball.GetRadius(), 0.00001, "Check radius");
+            Assert.AreEqual(50.0, ball.GetMass(), 0.00001, "Check mass");
+            Assert.AreEqual(33.51032, ball.GetVolume(), 0.00001, "Check volume");
+            Assert.AreEqual(50.26548, ball.GetSurfaceArea(), 0.00001, "Check area");
+            Assert.AreEqual(1.49208, ball.GetDensity(), 0.00001, "Check density");
+        }
+    }
+}
