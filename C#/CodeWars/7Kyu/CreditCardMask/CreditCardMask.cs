137e34fb2cd400df6c55d53e548c68af7f63c7a1
angeloLib <angelo.liborio.dev@gmail.com>
2025-04-03 00:57:51 +0100

    - creditCard

---
+++ C#/CodeWars/7Kyu/CreditCardMask/CreditCardMask.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Usually when you buy something, you're asked whether your credit card number,
+ * phone number or answer to your most secret question is still correct.
+ * 
+ * However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+*/
+
+using System;
+using System.Linq;
+using System.Text;
+using CodeWars._7Kyu.CreditCardMask;
+
+namespace CodeWars._7Kyu.CreditCardMask
+{
+    public static class CreditCardMask
+    {
+        private const char replaceValue = '#';
+        private const int lookupValue = 4;
+        public static string Maskify(string cc)
+        {
+            int timesReplaceValue = (cc.Length - lookupValue);
+
+            return cc.Length <= 4 ? cc : new string(
+                cc.Substring(0, timesReplaceValue).Select(c => replaceValue).ToArray()) + cc.Substring(timesReplaceValue, (cc.Length) - (timesReplaceValue));
+        }
+    }
+}
---
+++ C#/CodeWarsTests/7Kyu/CreditCardMask/CreditCardMaskTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars._7Kyu.CreditCardMask;
+
+namespace CodeWarsTests._7Kyu.MaskifyTests
+{
+    [TestClass]
+    public class CreditCardMaskTests
+    {
+        [TestMethod]
+        public void ExamplesTests()
+        {
+            Assert.AreEqual("############5616", CreditCardMask.Maskify("4556364607935616"));
+            Assert.AreEqual("1", CreditCardMask.Maskify("1"));
+            Assert.AreEqual("#1111", CreditCardMask.Maskify("11111"));
+        }
+    }
+}
